138dbb2cb770e9eee7b15cb66429575f988cef4c
unknown <zcfshr@gmail.com>
2022-12-25 16:19:50 -0700

    changed auth.js in server

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+
+
 function App() {
   return (
     <Router>
