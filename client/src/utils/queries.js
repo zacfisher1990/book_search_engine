132b46af0506f59343282a858f8c81479ee21568
unknown <zcfshr@gmail.com>
2022-12-25 17:51:28 -0700

    queries and mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,18 @@
+import { gpl } from "@apollo/client";
+
+export const GET_ME = gql`
+{
+    me {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+}`;
